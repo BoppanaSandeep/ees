3df50b95a36cf36d1afa034fa79c081156e72d2f
BoppanaSandeep <boppanasandeep57@gmail.com>
2018-05-20 21:23:01 +0530

    Added sms plugin.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AndroidPermissions,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, NavParams, Platform } from 'ionic-angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+declare var SMS: any;
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
+
 export class ListPage {
+  
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  messages: any = [];
+  items: Array<{ title: string, note: string, icon: string }>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public androidPermissions: AndroidPermissions, public platform: Platform) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
     for (let i = 1; i < 11; i++) {
@@ -34,4 +38,50 @@ export class ListPage {
       item: item
     });
   }
+
+  checkPermission() {
+    this.androidPermissions.checkPermission
+      (this.androidPermissions.PERMISSION.READ_SMS).then(
+        success => {
+
+          //if permission granted
+          this.ReadSMSList();
+        },
+        err => {
+
+          this.androidPermissions.requestPermission
+            (this.androidPermissions.PERMISSION.READ_SMS).
+            then(success => {
+              this.ReadSMSList();
+            },
+              err => {
+                alert("cancelled")
+              });
+        });
+
+    this.androidPermissions.requestPermissions
+      ([this.androidPermissions.PERMISSION.READ_SMS]);
+
+  }
+  ReadSMSList() {
+
+    this.platform.ready().then((readySource) => {
+
+      let filter = {
+        box: 'inbox', // 'inbox' (default), 'sent', 'draft'
+        indexFrom: 0, // start from index 0
+        maxCount: 20, // count of SMS to return each time
+      };
+
+      if (SMS) SMS.listSMS(filter, (ListSms) => {
+        this.messages = ListSms
+      },
+
+        Error => {
+          alert(JSON.stringify(Error))
+        });
+
+    });
+  }
+
 }
